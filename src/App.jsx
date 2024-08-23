859529b5de6e0e509d733d97c25aa7a6af62cc79
sol9028 <28solsol@naver.com>
2024-08-23 22:38:46 +0900

    충돌해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Router from "../Router";
 
 function App() {
+<<<<<<< Updated upstream
   return (
     <>
       <Router />
     </>
   );
+=======
+  return <></>;
+>>>>>>> Stashed changes
 }
 
 export default App;
